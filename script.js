11e7faec584219cb5b64b7cd6584115e44da8445
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-10-19 16:23:25 -0400

    name persistence commit 2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function nextpage(e){
     e.preventDefault();
+    localStorage.setItem("nameVal", document.getElementById("nameVal").value);
     window.location.href='serverSelect/index.html';
 }
 document.getElementById("submitButton").addEventListener("click", nextpage);
