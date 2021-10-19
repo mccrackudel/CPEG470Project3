0178f3e844c2baf85efc14de1d9fc97f5e2cb843
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-10-19 19:18:17 -0400

    testing

--- serverSelect/script.js
+++ serverSelect/script.js
@@ -1,3 +1,25 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
+import * as fb from  "https://www.gstatic.com/firebasejs/9.0.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJFmJSQEeZXx0qequ9i6_3JEgs66wsDX0",
+  authDomain: "chatroom-fed7d.firebaseapp.com",
+  databaseURL: "https://chatroom-fed7d-default-rtdb.firebaseio.com",
+  projectId: "chatroom-fed7d",
+  storageBucket: "chatroom-fed7d.appspot.com",
+  messagingSenderId: "880533252950",
+  appId: "1:880533252950:web:7f4dacbaf1208220da04f7"
+};
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+
+function tester(e){
+    e.preventDefault();
+    alert(database);
+
+}
+
 function nextpage(e){
     e.preventDefault();
     localStorage.setItem("serverVal", document.getElementById("serverVal").value);
