0c24f07575f396ef072be832e9296886d7867bae
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-10-19 14:47:57 -0400

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function nextpage(e){
     e.preventDefault();
-    window.location.href='serverSelect/index.html';
+    window.location.href='https://github.com/mccrackudel/CPEG470Project3/blob/ae597c960e8499861d90e002b40c0c6ca1ef979c/serverSelect/index.html';
 }
 document.getElementById("submitButton").addEventListener("click", nextpage);
