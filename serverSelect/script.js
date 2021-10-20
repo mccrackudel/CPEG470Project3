48f282be8daddfb7daca2c44ecfce99fc695a237
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-10-19 21:05:19 -0400

    server listings complete

--- serverSelect/script.js
+++ serverSelect/script.js
@@ -27,7 +27,7 @@ fb.onValue(titleRef, ss=>{
 
 function nextpage(e){
     e.preventDefault();
-    localStorage.setItem("serverVal", document.getElementById("serverVal").value);
+    localStorage.setItem("serverVal", document.getElementById("serverVal").value +"/");
     window.location.href='../serverDisplay/index.html';
 }
 document.getElementById("submitButton").addEventListener("click", nextpage);
